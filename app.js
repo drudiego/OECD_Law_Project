81919b62850e74c8bba7f4de5ed08f1c7be29d3a
Diego Dru <diegodru@gmail.com>
2023-12-20 08:00:46 +0100

    Preparing to Render
    added dotenv, MongoStore, sent database to MongoDBAtlas, setting up for Render

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+if (process.env.NODE_ENV !== "production") {
+    require('dotenv').config();
+}
+
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
@@ -8,6 +12,8 @@ const flash = require('connect-flash');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const User = require('./models/user')
+const MongoStore = require('connect-mongo')
+const dbUrl = process.env.DB_URL
 
 const ExpressError = require('./utils/ExpressError');
 
@@ -15,7 +21,7 @@ const userRoutes = require('./routes/users');
 const statementRoutes = require('./routes/statements');
 const segmentRoutes = require('./routes/segments');
 
-mongoose.connect('mongodb://127.0.0.1:27017/oecd-case-law');
+mongoose.connect(dbUrl);
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
@@ -33,8 +39,24 @@ app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
 
+const secret = process.env.SECRET || 'thisshouldbeabettersecret!';
+
+const store = MongoStore.create({
+    mongoUrl: dbUrl,
+    touchAfter: 24 * 60 * 60,
+    crypto: {
+        secret
+    }
+});
+
+store.on("error", function (e) {
+    console.log("SESSION STORE ERROR!")
+})
+
 const sessionConfig = {
-    secret: 'thissecret',
+    store,
+    name: 'session',
+    secret,
     resave: false,
     saveUninitialized: true,
     cookie: {
