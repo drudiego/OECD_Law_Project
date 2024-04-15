fcaba2aacdbc0243ed559a1c7c905b1ad63840a1
Diego Dru <diegodru@gmail.com>
2024-04-15 10:48:16 +0200

    started adding tags to the entry cards on the index page

--- controllers/statements.js
+++ controllers/statements.js
@@ -1,174 +1,203 @@
-const Statement = require('../models/statement');
-const Segment = require('../models/segment');
-const Fuse = require('fuse.js');
+const Statement = require("../models/statement");
+const Segment = require("../models/segment");
+const Fuse = require("fuse.js");
 const { cloudinary } = require("../cloudinary");
-const countriesData = require('../utils/countries.json');
-const { filterCategories } = require('../utils/filters.js')
+const countriesData = require("../utils/countries.json");
+const { filterCategories } = require("../utils/filters.js");
 
 module.exports.index = async (req, res) => {
-    const statements = await Statement.find({}).populate("segments");
-    const filters = filterCategories;
-    const data = countriesData
-    const countryCount = await Segment.aggregate([
-        { $match: { filter: 'Host country (Where the violations conducted)' } },
-        { $group: { _id: '$subfilter', count: { $sum: 1 } } },
-      ])
-    // console.log(countryCount)
-    res.render('statements/index', { statements, filters, countryCount, data })
-
+  const statements = await Statement.find({}).populate("segments");
+  const filters = filterCategories;
+  const data = countriesData;
+  const segments = await Segment.find({});
+  const countryCount = await Segment.aggregate([
+    { $match: { filter: "Host country (Where the violations conducted)" } },
+    { $group: { _id: "$subfilter", count: { $sum: 1 } } },
+  ]);
+  // console.log(countryCount)
+  res.render("statements/index", {
+    statements,
+    filters,
+    countryCount,
+    data,
+    segments,
+  });
 };
 
 module.exports.renderAbout = (req, res) => {
-    res.render('statements/about')
+  res.render("statements/about");
 };
 
 module.exports.renderStaff = (req, res) => {
-    res.render('statements/staff')
+  res.render("statements/staff");
 };
 
 module.exports.renderNewForm = (req, res) => {
-    const filters = filterCategories;
-    res.render('statements/new', { filters })
+  const filters = filterCategories;
+  res.render("statements/new", { filters });
 };
 
 module.exports.createStatement = async (req, res) => {
-    // console.log(req.body.statement)
-    
-    const statement = new Statement(req.body.statement);
-    statement.author = req.user._id;
-    
-    console.log(req.files)
-    statement.pdfFile = req.files.map(f => ({url: f.path, filename: f.filename     
-    }))
-    await statement.save();
-    // console.log(statement)
-    req.flash('success', 'Successfully made a new Statement');
-    res.redirect(`/statements/${statement._id}`)
+  // console.log(req.body.statement)
+
+  const statement = new Statement(req.body.statement);
+  statement.author = req.user._id;
+
+  console.log(req.files);
+  statement.pdfFile = req.files.map((f) => ({
+    url: f.path,
+    filename: f.filename,
+  }));
+  await statement.save();
+  // console.log(statement)
+  req.flash("success", "Successfully made a new Statement");
+  res.redirect(`/statements/${statement._id}`);
 };
 
 module.exports.search = async (req, res, next) => {
-    // Extract the search term and selected filters from the request query
-    const searchTerm = req.query.searchTerm;
-    const selectedFilters = req.query.selectedFilters;
+  // Extract the search term and selected filters from the request query
+  const searchTerm = req.query.searchTerm;
+  const selectedFilters = req.query.selectedFilters;
 
-    // Define the filter categories
-    const filters = filterCategories;
+  // Define the filter categories
+  const filters = filterCategories;
 
-    // Initialize arrays to store the results, matching segments, and matching segment IDs
-    const results = [];
-    const matchingSegments = {};
-    const matchingSegmentIds = {};
+  // Initialize arrays to store the results, matching segments, and matching segment IDs
+  const results = [];
+  const matchingSegments = {};
+  const matchingSegmentIds = {};
 
-    // Iterate through the selected filters
-    for (const filter in selectedFilters) {
-        // Ensure that the selected filters are always an array
-        if (selectedFilters[filter].length > 0) {
-            selectedFilters[filter] = Array.isArray(selectedFilters[filter]) ? selectedFilters[filter] : [selectedFilters[filter]];
-        }
-        // Find segments that match the selected filters and store them in the matchingSegments object separated by the filter
-        matchingSegments[filter] = await Segment.find({ subfilter: { $in: selectedFilters[filter] } }).lean();
-        // Extract the IDs of the matching segments and store them in the matchingSegmentIds object separated by the filter
-        matchingSegmentIds[filter] = matchingSegments[filter].map(segment => segment._id);
+  // Iterate through the selected filters
+  for (const filter in selectedFilters) {
+    // Ensure that the selected filters are always an array
+    if (selectedFilters[filter].length > 0) {
+      selectedFilters[filter] = Array.isArray(selectedFilters[filter])
+        ? selectedFilters[filter]
+        : [selectedFilters[filter]];
     }
+    // Find segments that match the selected filters and store them in the matchingSegments object separated by the filter
+    matchingSegments[filter] = await Segment.find({
+      subfilter: { $in: selectedFilters[filter] },
+    }).lean();
+    // Extract the IDs of the matching segments and store them in the matchingSegmentIds object separated by the filter
+    matchingSegmentIds[filter] = matchingSegments[filter].map(
+      (segment) => segment._id
+    );
+  }
 
-    let entries;
+  let entries;
 
-    // If there are matching segments, construct the final query using the matching segment IDs with different arrays for each filter
-    if (Object.keys(matchingSegments).length > 0) {
-        const finalQuery = Object.values(matchingSegmentIds).map(ids => ({ 'segments': { $in: ids } }));
-        // Find statements that match the final query using the $and operator to ensure that all filters are met
-        entries = await Statement.find({ $and: finalQuery }).lean();
-    } else {
-        // If there are no matching segments, find all statements
-        entries = await Statement.find().lean();
-    }
+  // If there are matching segments, construct the final query using the matching segment IDs with different arrays for each filter
+  if (Object.keys(matchingSegments).length > 0) {
+    const finalQuery = Object.values(matchingSegmentIds).map((ids) => ({
+      segments: { $in: ids },
+    }));
+    // Find statements that match the final query using the $and operator to ensure that all filters are met
+    entries = await Statement.find({ $and: finalQuery }).lean();
+  } else {
+    // If there are no matching segments, find all statements
+    entries = await Statement.find().lean();
+  }
 
-    // If a search term is provided, perform a fuzzy search using Fuse.js
-    if (searchTerm !== "") {
-        // Split the search term into an array of individual terms
-        const searchTermArray = searchTerm.split(' ').join('|');
-        // Create a new instance of Fuse.js with the entries as the data source and specified search keys and options
-        const fuse = new Fuse(entries, {
-            keys: [
-                { name: 'title', weight: 2 },
-                { name: 'summary', weight: 1 }
-            ],
-            useExtendedSearch: true,
-            includeScore: true,
-            ignoreLocation: true,
-            threshold: 0.3,
-        });
-        // Perform the fuzzy search using the search term array and add the results to the results array
-        results.push(...fuse.search(searchTermArray));
-    } else {
-        // If no search term is provided, simply add each entry to the results array
-        entries.forEach(entry => {
-            results.push({ item: entry });
-        });
-    }
+  // If a search term is provided, perform a fuzzy search using Fuse.js
+  if (searchTerm !== "") {
+    // Split the search term into an array of individual terms
+    const searchTermArray = searchTerm.split(" ").join("|");
+    // Create a new instance of Fuse.js with the entries as the data source and specified search keys and options
+    const fuse = new Fuse(entries, {
+      keys: [
+        { name: "title", weight: 2 },
+        { name: "summary", weight: 1 },
+      ],
+      useExtendedSearch: true,
+      includeScore: true,
+      ignoreLocation: true,
+      threshold: 0.3,
+    });
+    // Perform the fuzzy search using the search term array and add the results to the results array
+    results.push(...fuse.search(searchTermArray));
+  } else {
+    // If no search term is provided, simply add each entry to the results array
+    entries.forEach((entry) => {
+      results.push({ item: entry });
+    });
+  }
 
-    // Render the search results page with the results, filters, selected filters, and search term
-    res.render('statements/searchResults', { results, filters, selectedFilters, searchTerm });
+  // Render the search results page with the results, filters, selected filters, and search term
+  res.render("statements/searchResults", {
+    results,
+    filters,
+    selectedFilters,
+    searchTerm,
+  });
 };
 
 module.exports.showStatement = async (req, res) => {
-    const statement = await Statement.findById(req.params.id).populate("segments").populate("author");
-    const filters = filterCategories;
-    if (!statement) {
-        req.flash('error', 'Cannot find that Statement');
-        return res.redirect('/statements')
-    }
-    res.render('statements/show', { statement, filters })
+  const statement = await Statement.findById(req.params.id)
+    .populate("segments")
+    .populate("author");
+  const filters = filterCategories;
+  if (!statement) {
+    req.flash("error", "Cannot find that Statement");
+    return res.redirect("/statements");
+  }
+  res.render("statements/show", { statement, filters });
 };
 
 module.exports.renderEditForm = async (req, res) => {
-    const statement = await Statement.findById(req.params.id);
-    const filters = filterCategories;
-    if (!statement) {
-        req.flash('error', 'Cannot find that Statement');
-        return res.redirect('/statements')
-    }
-    res.render('statements/edit', { statement, filters })
+  const statement = await Statement.findById(req.params.id);
+  const filters = filterCategories;
+  if (!statement) {
+    req.flash("error", "Cannot find that Statement");
+    return res.redirect("/statements");
+  }
+  res.render("statements/edit", { statement, filters });
 };
 
 module.exports.renderEditSegmentsForm = async (req, res) => {
-    console.log("Your params:", req.params)
+  console.log("Your params:", req.params);
 
-    const statement = await Statement.findById(req.params.id).populate("segments").populate("author");
-    const filters = filterCategories;
-    if (!statement) {
-        req.flash('error', 'Cannot find that Statement');
-        return res.redirect('/statements')
-    }
-    res.render('statements/editSegments', { statement, filters })
+  const statement = await Statement.findById(req.params.id)
+    .populate("segments")
+    .populate("author");
+  const filters = filterCategories;
+  if (!statement) {
+    req.flash("error", "Cannot find that Statement");
+    return res.redirect("/statements");
+  }
+  res.render("statements/editSegments", { statement, filters });
 };
 
 module.exports.updateStatement = async (req, res) => {
-    const statement = await Statement.findByIdAndUpdate(req.params.id, { ...req.body.statement });
-    console.log(req.body)
-    // console.log(req.files)
-    const files = req.files.map(f => (
-        {url: f.path, filename: f.filename, originalname: f.originalname   
+  const statement = await Statement.findByIdAndUpdate(req.params.id, {
+    ...req.body.statement,
+  });
+  console.log(req.body);
+  // console.log(req.files)
+  const files = req.files.map((f) => ({
+    url: f.path,
+    filename: f.filename,
+    originalname: f.originalname,
+  }));
+  // console.log(files)
+  statement.pdfFile.push(...files);
+  await statement.save();
+  if (req.body.deleteFiles) {
+    for (let filename of req.body.deleteFiles) {
+      if (filename) {
+        await cloudinary.uploader.destroy(filename);
+      }
     }
-    ))
-    // console.log(files)
-    statement.pdfFile.push(...files)
-    await statement.save();
-    if(req.body.deleteFiles){
-        for (let filename of req.body.deleteFiles) {
-            if (filename){
-                await cloudinary.uploader.destroy(filename);
-            }
-        }
-        await statement.updateOne({ $pull: { pdfFile: { filename: { $in: req.body.deleteFiles } } }})
-        
-    };
-    req.flash('success', 'Successfully updated the Statement');
-    res.redirect(`/statements/${statement._id}`)
+    await statement.updateOne({
+      $pull: { pdfFile: { filename: { $in: req.body.deleteFiles } } },
+    });
+  }
+  req.flash("success", "Successfully updated the Statement");
+  res.redirect(`/statements/${statement._id}`);
 };
 
 module.exports.deleteStatement = async (req, res) => {
-    await Statement.findByIdAndDelete(req.params.id);
-    req.flash('success', 'Successfully deleted the Statement');
-    res.redirect('/statements')
+  await Statement.findByIdAndDelete(req.params.id);
+  req.flash("success", "Successfully deleted the Statement");
+  res.redirect("/statements");
 };
