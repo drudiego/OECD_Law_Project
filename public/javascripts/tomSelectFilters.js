17b189740d71f00988bfabecdfac1265b1cac346
Diego Dru <diegodru@gmail.com>
2024-04-21 10:11:01 +0200

    changed layout of Tom Select when field is selected (using dropdown_header)

--- public/javascripts/tomSelectFilters.js
+++ public/javascripts/tomSelectFilters.js
@@ -1,31 +1,32 @@
-
 // Initialize Tom Select
-document.querySelectorAll('.select').forEach((el) => {
-    new TomSelect(el, {
-        plugins: [
-            { name: 'remove_button', options: { title: "Remove option" } },
-            { name: 'clear_button', options: { title: "Remove all options" } },
-            {
-                name: 'checkbox_options', options: {
-                    'checkedClassNames': ['ts-checked'],
-                    'uncheckedClassNames': ['ts-unchecked'],
-                }
-            },
-
-        ],
-        persist: false,
-        create: false,
-        onItemAdd: function () {
-            this.setTextboxValue('');
-            this.refreshOptions();
+document.querySelectorAll(".select").forEach((el) => {
+  new TomSelect(el, {
+    plugins: [
+      { name: "remove_button", options: { title: "Remove option" } },
+      { name: "dropdown_header", options: { title: "Select options" } },
+      {
+        name: "checkbox_options",
+        options: {
+          checkedClassNames: ["ts-checked"],
+          uncheckedClassNames: ["ts-unchecked"],
         },
-        render: {
-            option: function (data, escape) {
-                return '<div class="d-flex"><span>' + escape(data.value) + '</span></div>';
-            },
-            item: function (data, escape) {
-                return '<div>' + escape(data.value) + '</div>';
-            }
-        }
-    });
-})
+      },
+    ],
+    persist: false,
+    create: false,
+    onItemAdd: function () {
+      this.setTextboxValue("");
+      this.refreshOptions();
+    },
+    render: {
+      option: function (data, escape) {
+        return (
+          '<div class="d-flex"><span>' + escape(data.value) + "</span></div>"
+        );
+      },
+      item: function (data, escape) {
+        return "<div>" + escape(data.value) + "</div>";
+      },
+    },
+  });
+});
