1b4b146272c8c86d695245dd79d401fb6fc2f2b3
Diego Dru <diegodru@gmail.com>
2024-08-17 13:44:45 +0200

    Added content to the About the Project and Code Handbook pages. Not yet styled.

--- routes/statements.js
+++ routes/statements.js
@@ -1,39 +1,55 @@
 const express = require(`express`);
 const router = express.Router();
 
-const statements = require('../controllers/statements');
-const catchAsync = require('../utils/catchAsync');
-const { isLoggedIn, validateStatement } = require('../middleware.js');
-const multer = require('multer');
-const { storage } = require('../cloudinary');
+const statements = require("../controllers/statements");
+const catchAsync = require("../utils/catchAsync");
+const { isLoggedIn, validateStatement } = require("../middleware.js");
+const multer = require("multer");
+const { storage } = require("../cloudinary");
 const upload = multer({ storage });
 // const upload = multer({ dest: 'uploads/' });
 
-router.route('/')
-    .get(catchAsync(statements.index))
-    .post(isLoggedIn, upload.array('pdfFile'),  validateStatement, catchAsync(statements.createStatement));
-    // .post(upload.single('pdfFile'), (req, res) => {
-    //     console.log(req.body, req.file)
-    //     res.send("Working")
-    // })
+router
+	.route("/")
+	.get(catchAsync(statements.index))
+	.post(
+		isLoggedIn,
+		upload.array("pdfFile"),
+		validateStatement,
+		catchAsync(statements.createStatement)
+	);
+// .post(upload.single('pdfFile'), (req, res) => {
+//     console.log(req.body, req.file)
+//     res.send("Working")
+// })
 
+router.get("/about", statements.renderAbout);
 
-router.get('/about', statements.renderAbout);
+router.get("/handbook", statements.renderHandbook);
 
-router.get('/staff', statements.renderStaff);
+router.get("/staff", statements.renderStaff);
 
-router.get('/new', isLoggedIn, statements.renderNewForm);
+router.get("/new", isLoggedIn, statements.renderNewForm);
 
-router.get('/search', catchAsync(statements.search));
+router.get("/search", catchAsync(statements.search));
 
-router.route('/:id')
-    .get(catchAsync(statements.showStatement))
-    .put(isLoggedIn, upload.array('pdfFile'),  validateStatement, catchAsync(statements.updateStatement))
-    .delete(isLoggedIn, catchAsync(statements.deleteStatement));
+router
+	.route("/:id")
+	.get(catchAsync(statements.showStatement))
+	.put(
+		isLoggedIn,
+		upload.array("pdfFile"),
+		validateStatement,
+		catchAsync(statements.updateStatement)
+	)
+	.delete(isLoggedIn, catchAsync(statements.deleteStatement));
 
-router.get('/:id/edit', isLoggedIn, catchAsync(statements.renderEditForm));
+router.get("/:id/edit", isLoggedIn, catchAsync(statements.renderEditForm));
 
-router.get('/:id/editSegments', isLoggedIn, catchAsync(statements.renderEditSegmentsForm))
+router.get(
+	"/:id/editSegments",
+	isLoggedIn,
+	catchAsync(statements.renderEditSegmentsForm)
+);
 
-
-module.exports = router
+module.exports = router;
