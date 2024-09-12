ff3c48577c24f9063114cf8acea4f8f8a96c0869
Diego Dru <diegodru@gmail.com>
2024-09-12 11:05:04 +0200

    Created the script to populate the segments database

---
+++ seeds/csvp.js
@@ -0,0 +1,30 @@
+module.exports.seg = [
+	{
+		statement: "66e298e27466ceb37d41ca5c",
+		filter: "Risk-based due diligence process under OECD Guidelines ",
+		subfilter: " Identification/Assessment of Adverse Impacts",
+		body: "Information   about the local regulatory framework,   governance, socio-economic context,   and political context may also be rele-vant to examine during the identification   and assessment of actual and potential   adverse impacts.",
+		page: "7",
+	},
+	{
+		statement: "66e298e27466ceb37d41ca5c",
+		filter: "Stakeholder Engagement ",
+		subfilter: " Definition/Identification of Stakeholders",
+		body: "Stakeholder engagement involves inter-active processes of engagement with rele-vant stakeholders through, for example,   meetings, hearings or consultation procee-dings. Meaningful stakeholder engagement   is characterised by two-way communi-cation and depends on the good faith of   the participants on both sides.",
+		page: "7",
+	},
+	{
+		statement: "66e298e27466ceb37d41ca5c",
+		filter: "Stakeholder Engagement ",
+		subfilter: " Effectiveness of Stakeholder Engagement",
+		body: "It is also   responsive and on-going, and includes   in many cases engaging with relevant   stakeholders before decisions have been  \n10 National Human Rights Institutions.  11 The stormwater discharge permit is being disputed by a local non-profit organization in a legal proceeding.8 \nmade, for example in the planning and  \ndecision-making concerning projects or   other activities involving the intensive use   of land or water, which could significantly   affect local communities.12",
+		page: "7",
+	},
+	{
+		statement: "66e298e27466ceb37d41ca5c",
+		filter: "Stakeholder Engagement ",
+		subfilter: " Effectiveness of Stakeholder Engagement",
+		body: "Con-sultation with potentially affected stake-holders can require particular sensitivity.   Some individuals or groups may be at risk   of exclusion from the consultation pro-cess unless targeted efforts are made to   reach out to them. There may be com-peting views among and within stakeholder   groups about the relative significance of   certain impacts.13",
+		page: "8",
+	},
+];
--- seeds/segmentsScript.js
+++ seeds/segmentsScript.js
@@ -16,13 +16,15 @@ db.once("open", () => {
 const seedDB = async () => {
 	await Segment.deleteMany();
 	for (s of seg) {
+		console.log(s);
 		const statement = await Statement.findById(s.statement);
+		console.log(statement);
 		const segment = new Segment({
-			statementTitle: statement.title,
 			body: s.body,
 			filter: s.filter,
 			subfilter: s.subfilter,
-			page: s.page,
+			statementTitle: statement.title,
+			// page: s.page,
 		});
 		statement.segments.push(segment);
 		await segment.save();
